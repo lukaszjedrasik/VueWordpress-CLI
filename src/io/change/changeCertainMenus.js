f3e0b10782679c6cdcb45208c105e5f19552e81a
Łukasz <lukasz.jedrasik@icloud.com>
2019-06-23 16:09:03 +0200

    Survey for PWA

---
+++ src/io/change/changeCertainMenus.js
@@ -0,0 +1,27 @@
+module.exports = (mainFile, data) => {
+  let lang = /(lang:)\s.+/g;
+  lang = mainFile.match(lang);
+  let content;
+  const exp = /(menus:)\s.+/g;
+  const pass = mainFile.match(exp);
+  if (pass) {
+    expression = /(menus:)\s.+/g;
+    newFile = mainFile.replace(
+      expression,
+      data.menus.length === 1
+        ? `menus: '${data.menus}'`
+        : `menus: [${data.menus.map(v => `"${v}"`)}]`
+    );
+  } else {
+    const newMenus =
+      data.menus.length === 1
+        ? `menus: '${data.menus}'`
+        : `menus: [${data.menus.map(v => `"${v}"`)}]`;
+
+    content = `${lang},
+    ${newMenus}`;
+
+    newFile = mainFile.replace(lang, content);
+  }
+  return newFile;
+};
--- src/io/change/changeFileJsTs.js
+++ src/io/change/changeFileJsTs.js
@@ -1,3 +1,5 @@
+const changeCertainMenu = require("./changeCertainMenus");
+
 module.exports = (mainFile, data) => {
   const newData = () => {
     let newFile;
@@ -7,11 +9,23 @@ module.exports = (mainFile, data) => {
       newFile = mainFile.replace(expression, `url: '${data.url}',`);
       return newFile;
     } else if (data.lang) {
-      expression = /(lang:)\s('|")[a-zA-z]{2}('|")(,)/gi;
-      newFile = mainFile.replace(expression, `lang: '${data.lang}',`);
+      expression = /(lang:)\s('|")[a-zA-z]{2}('|")/gi;
+      newFile = mainFile.replace(expression, `lang: '${data.lang}'`);
       return newFile;
-    } else {
-      return data;
+    } else if (data.menuFetchMode) {
+      if (data.menuFetchMode === "each") {
+        expression = /(menus:)\s.+/g;
+        newFile = mainFile.replace(expression, "");
+        return newFile;
+      } else if (data.menuFetchMode === "certain") {
+        const changeCertain = changeCertainMenu(mainFile, data);
+        newFile = changeCertain;
+        return newFile;
+      } else if (data.menuFetchMode === "none") {
+        // to do
+        return "is none";
+      }
+      return newFile;
     }
   };
 
